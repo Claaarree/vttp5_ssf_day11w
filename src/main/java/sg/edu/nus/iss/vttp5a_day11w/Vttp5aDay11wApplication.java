ee1d3c9d1f6df41776503857e5ecc9ca8569eaee
Claaarree <clare.fh@hotmail.com>
2024-11-14 21:47:04 +0800

    half of ws1

---
+++ src/main/java/sg/edu/nus/iss/vttp5a_day11w/Vttp5aDay11wApplication.java
@@ -0,0 +1,31 @@
+package sg.edu.nus.iss.vttp5a_day11w;
+
+import java.util.Collections;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.DefaultApplicationArguments;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aDay11wApplication {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(Vttp5aDay11wApplication.class, args);
+		SpringApplication app = new SpringApplication(Vttp5aDay11wApplication.class);
+		ApplicationArguments appArgs = new DefaultApplicationArguments(args);
+		String port = "3000";
+
+		// if (appArgs.containsOption("port")){
+		// 	port = appArgs.getOptionValues("port").get(0);
+		// }else if (!System.getenv("PORT").equals(null)){
+		// 	port = System.getenv("PORT");
+		// }
+		//System.out.println(System.getenv("PORT"));
+
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+
+		app.run(args);
+	}
+
+}
